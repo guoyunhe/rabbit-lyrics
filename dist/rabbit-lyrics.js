cff54fd1e272ceae61d1b968d525e2e1d1b4693e
Guo Yunhe <guoyunhebrave@gmail.com>
2017-07-29 15:43:10 +0300

    Rewrite as NPM module

--- dist/rabbit-lyrics.js
+++ dist/rabbit-lyrics.js
@@ -1,3 +1,76 @@
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, {
+/******/ 				configurable: false,
+/******/ 				enumerable: true,
+/******/ 				get: getter
+/******/ 			});
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = 0);
+/******/ })
+/************************************************************************/
+/******/ ([
+/* 0 */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__styles_main_scss__ = __webpack_require__(1);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__styles_main_scss___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_0__styles_main_scss__);
 /*
  * Rabbit Lyrics
  *
@@ -10,7 +83,7 @@
  * Report bugs: https://github.com/guoyunhe/rabbit-lyrics/issues
  */
 
-import './styles/main.scss'
+ 
 
 /**
  * Rabbit Lyrics main class
@@ -28,7 +101,7 @@ import './styles/main.scss'
  *      Timer in milliseconds for scrolling animation. This is not an option
  *      but a status value that dynamically changes when playing
  */
-export default class RabbitLyrics {
+class RabbitLyrics {
 
     /**
      * Constructor
@@ -220,6 +293,8 @@ export default class RabbitLyrics {
         return time;
     }
 }
+/* harmony export (immutable) */ __webpack_exports__["default"] = RabbitLyrics;
+
 
 // Define global variable
 window.RabbitLyrics = RabbitLyrics;
@@ -228,7 +303,7 @@ window.RabbitLyrics = RabbitLyrics;
 document.addEventListener('DOMContentLoaded', function () {
     let elements = document.getElementsByClassName('rabbit-lyrics')
 
-    for (let i = 0; i < elements.length; i++) {
+    for (let i = 0 ; i < elements.length; i++) {
         new RabbitLyrics({
             element: elements[i]
         });
@@ -236,3 +311,13 @@ document.addEventListener('DOMContentLoaded', function () {
 }, false);
 
 // TODO Support jQuery
+
+
+/***/ }),
+/* 1 */
+/***/ (function(module, exports) {
+
+// removed by extract-text-webpack-plugin
+
+/***/ })
+/******/ ]);
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,32 @@
 const path = require('path');
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+
+const extractSass = new ExtractTextPlugin({
+    filename: "rabbit-lyrics.css",
+    disable: process.env.NODE_ENV === "development"
+});
 
 module.exports = {
     entry: './src/index.js',
     output: {
         path: path.resolve(__dirname, 'dist'),
-        filename: 'bundle.js'
-    }
+        filename: 'rabbit-lyrics.js'
+    },
+    module: {
+        rules: [{
+            test: /\.scss$/,
+            use: extractSass.extract({
+                use: [{
+                    loader: "css-loader"
+                }, {
+                    loader: "sass-loader"
+                }],
+                // use style-loader in development
+                fallback: "style-loader"
+            })
+        }]
+    },
+    plugins: [
+        extractSass
+    ]
 };
